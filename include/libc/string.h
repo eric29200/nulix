e03b273f463aab13223e3da5780e037bc75439b1
eric29200 <eric_begot@yahoo.fr>
2021-03-27 15:23:57 +0100

    add string.c functions

--- include/libc/string.h
+++ include/libc/string.h
@@ -3,6 +3,16 @@
 
 #include <libc/stddef.h>
 
+size_t strlen(const char *s);
+size_t strnlen(const char *s, size_t maxlen);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
+char *strchr(const char *s, char c);
+
 void memset(void *s, char c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
 
 #endif
