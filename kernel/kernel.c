e801334632318391258f4de278d5ce175832fd70
eric29200 <eric_begot@yahoo.fr>
2021-03-28 21:21:13 +0200

    memory start = end of static kernel code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,12 +9,14 @@
 #define TIMER_HZ      50
 
 extern uint32_t loader;
+extern uint32_t kernel_end;
 
 int kmain(unsigned long magic, multiboot_info_t *mboot)
 {
   /* check multiboot */
   if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
     return 0xD15EA5E;
+
 
   /* clear screen */
   screen_clear();
@@ -34,9 +36,9 @@ int kmain(unsigned long magic, multiboot_info_t *mboot)
   printf("[Kernel] Timer Init at %dHz\n", TIMER_HZ);
   init_timer(TIMER_HZ);
 
-  /* init paging */
+  /* init paging (start at end of static kernel code and finish at end of memory) */
   printf("[Kernel] Memory paging Init\n");
-  init_mem(0x300001, 0x100000 + mboot->mem_upper * 1024);
+  init_mem((uint32_t) &kernel_end, 0x100000 + mboot->mem_upper * 1024);
 
   /* enable interrupts */
   __asm__("sti");
