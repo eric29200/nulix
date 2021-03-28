0b323af8324d86c3fe34fd86f9c87a1c73e0ab98
eric29200 <eric_begot@yahoo.fr>
2021-03-28 13:25:56 +0200

    use grub to get memory available

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,11 +2,38 @@
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
 #include <kernel/timer.h>
+#include <grub/multiboot.h>
+#include <libc/stdio.h>
 
-int kmain()
+extern uint32_t loader;
+
+int kmain(unsigned long magic, multiboot_info_t *mboot)
 {
+  multiboot_memory_map_t *mmap;
+  uint32_t total_mem;
+
+  /* check multiboot */
+  if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
+    return 0xD15EA5E;
+
   /* clear screen */
   screen_clear();
+
+  /* get total memory available */
+  mmap = (multiboot_memory_map_t *) mboot->mmap_addr;
+  while (mmap < (multiboot_memory_map_t *) (mboot->mmap_addr + mboot->mmap_length)) {
+    if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE)
+      total_mem += mmap->len;
+
+    mmap = (multiboot_memory_map_t *) ((uint32_t) mmap + mmap->size + sizeof(uint32_t));
+  }
+
+  /* print grub informations */
+  printf("Kernel loading linear address = %x\n", loader);
+  printf("Grub magic = %x\n", magic);
+  if (mboot->flags & (1 << 2))
+    printf("Grub cmdline = %s\n", (const char *) mboot->cmdline);
+  printf("Total Memory = %d kB\n", total_mem / 1024);
 
   /* init gdt */
   init_gdt();
@@ -15,7 +42,7 @@ int kmain()
   init_idt();
 
   /* init timer at 50 Hz */
-  init_timer(50);
+  //init_timer(50);
 
   /* enable interrupts */
   __asm__("sti");
