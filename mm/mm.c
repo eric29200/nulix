baaa4075f59fc8dd9fe0b73d147399143eaf99c1
eric29200 <eric_begot@yahoo.fr>
2021-04-11 16:51:28 +0200

    revert heap

--- mm/mm.c
+++ mm/mm.c
@@ -12,17 +12,30 @@ struct heap_t *kheap = NULL;
 /*
  * Allocate memory (internal function).
  */
-static void *__kmalloc(uint32_t size, uint8_t align)
+static void *__kmalloc(uint32_t size, uint8_t align, uint32_t *phys)
 {
+  struct page_t *page;
   void *ret;
 
   /* use kernel heap */
-  if (kheap)
-    return heap_alloc(kheap, size, align);
+  if (kheap) {
+    ret = heap_alloc(kheap, size, align);
+
+    if (phys) {
+      page = get_page((uint32_t) ret, 0, kernel_pgd);
+      *phys = page->frame * PAGE_SIZE + ((uint32_t) ret & 0xFFF);
+    }
+
+    return ret;
+  }
 
   /* align adress on PAGE boundary */
   if (align == 1 && !PAGE_ALIGNED(placement_address))
     placement_address = PAGE_ALIGN(placement_address);
+
+  /* get physical memory */
+  if (phys)
+    *phys = placement_address;
 
   /* update placement_address */
   ret = (void *) placement_address;
@@ -35,7 +48,7 @@ static void *__kmalloc(uint32_t size, uint8_t align)
  */
 void *kmalloc(uint32_t size)
 {
-  return __kmalloc(size, 0);
+  return __kmalloc(size, 0, NULL);
 }
 
 /*
@@ -43,28 +56,15 @@ void *kmalloc(uint32_t size)
  */
 void *kmalloc_align(uint32_t size)
 {
-  return __kmalloc(size, 1);
+  return __kmalloc(size, 1, NULL);
 }
 
 /*
- * Allocate page aligned memory to physical memory.
+ * Allocate page aligned memory and output physical memory.
  */
 void *kmalloc_align_phys(uint32_t size, uint32_t *phys)
 {
-  void *ret;
-
-  /* align adress on PAGE boundary */
-  if (!PAGE_ALIGNED(placement_address))
-    placement_address = PAGE_ALIGN(placement_address);
-
-  /* get physical memory */
-  if (phys)
-    *phys = placement_address;
-
-  /* update placement_address */
-  ret = (void *) placement_address;
-  placement_address += size;
-  return ret;
+  return __kmalloc(size, 1, phys);
 }
 
 /*
@@ -82,8 +82,6 @@ void kfree(void *p)
 void init_mem(uint32_t start, uint32_t end)
 {
   uint32_t max_nb_page_tables;
-  uint32_t kernel_nb_page_tables;
-  uint32_t kernel_area;
   uint32_t max_heap_size;
   uint32_t i;
 
@@ -116,11 +114,7 @@ void init_mem(uint32_t start, uint32_t end)
   for (i = KHEAP_START; i < KHEAP_START + KHEAP_INIT_SIZE; i += PAGE_SIZE)
     alloc_frame(get_page(i, 1, kernel_pgd), 0, 0);
 
-  /* compute max heap size (= based on maximum page tables size) */
-  kernel_area = placement_address + max_nb_page_tables * sizeof(struct page_table_t);
-  kernel_nb_page_tables = 1 + kernel_area / (1024 * PAGE_SIZE);
-  max_heap_size = (max_nb_page_tables - kernel_nb_page_tables) * 1024 * PAGE_SIZE;
-
   /* init heap */
+  max_heap_size = end - placement_address - max_nb_page_tables * sizeof(struct page_t);
   kheap = heap_create(KHEAP_START, KHEAP_START + max_heap_size, KHEAP_INIT_SIZE);
 }
