57a844fe0e195b75c6e6851f9d193d316c9c7714
eric29200 <eric_begot@yahoo.fr>
2021-04-25 20:38:19 +0200

    init : add syscall helpers

--- usr/init.c
+++ usr/init.c
@@ -1,3 +1,24 @@
+static inline int syscall1(int n, int p1)
+{
+  int ret;
+  __asm__ __volatile__("int $0x80" : "=a" (ret) : "0" (n), "b" ((int) p1));
+  return ret;
+}
+
+static inline int syscall2(int n, int p1, int p2)
+{
+  int ret;
+  __asm__ __volatile__("int $0x80" : "=a" (ret) : "0" (n), "b" ((int) p1), "c" ((int) p2));
+  return ret;
+}
+
+static inline int syscall3(int n, int p1, int p2, int p3)
+{
+  int ret;
+  __asm__ __volatile__("int $0x80" : "=a" (ret) : "0" (n), "b" ((int) p1), "c" ((int) p2), "d" ((int) p3));
+  return ret;
+}
+
 int main(void)
 {
   char *tty = "/dev/tty0";
@@ -5,14 +26,14 @@ int main(void)
   int fd, ret;
 
   /* open tty 0 */
-  __asm__ __volatile__("int $0x80" : "=a" (fd) : "0" (6), "b" ((int) tty));
+  fd = syscall1(6, (int) tty);
 
   if (fd >= 0) {
     /* write to tty0 */
-    __asm__ __volatile__("int $0x80" : "=a" (ret) : "0" (4), "b" ((int) fd), "c" ((int) msg), "d" ((int) 5));
+    syscall3(4, fd, (int) msg, 5);
 
     /* close tty 0 */
-    __asm__ __volatile__("int $0x80" : "=a" (ret) : "0" (7), "b" (fd));
+    syscall1(7, fd);
   }
 
   return 0;
