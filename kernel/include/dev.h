145cd4fa71faec1c489a421d54ab03d2df9a7845
eric29200 <eric_begot@yahoo.fr>
2021-04-28 09:23:19 +0200

    remove last tty

--- kernel/include/dev.h
+++ kernel/include/dev.h
@@ -5,7 +5,6 @@
 #define DEV_TTY1      0x401
 #define DEV_TTY2      0x402
 #define DEV_TTY3      0x403
-#define DEV_TTY4      0x404
 #define DEV_TTY       0x500     /* current process tty */
 
 #endif
