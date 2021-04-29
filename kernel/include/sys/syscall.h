2cfe6594fd7b702507d670a0a9aed7014a815bf9
eric29200 <eric_begot@yahoo.fr>
2021-04-29 20:25:22 +0200

    add stat system call

--- kernel/include/sys/syscall.h
+++ kernel/include/sys/syscall.h
@@ -10,15 +10,11 @@
 #define __NR_fork         2
 #define __NR_read         3
 #define __NR_write        4
-#define __NR_mknod        5
-#define __NR_open         6
-#define __NR_close        7
-#define __NR_waitpid      8
-#define __NR_creat        9
-#define __NR_dup          10
-#define __NR_dup2         11
-#define __NR_execve       12
-#define __NR_sbrk         13
+#define __NR_open         5
+#define __NR_close        6
+#define __NR_stat         7
+#define __NR_execve       8
+#define __NR_sbrk         9
 
 typedef int32_t (*syscall_f)(uint32_t nr, ...);
 
