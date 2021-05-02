c2826e7da079dc60f913a5b66a846214c32b85d0
eric29200 <eric_begot@yahoo.fr>
2021-05-02 21:24:37 +0200

    exeve : simplify code

--- kernel/sys/sys_execve.c
+++ kernel/sys/sys_execve.c
@@ -10,103 +10,110 @@
 extern void enter_user_mode(uint32_t esp, uint32_t eip, uint32_t return_address);
 
 /*
+ * Copy an array from user memory to kernel memory.
+ */
+static char **copy_array_from_user_to_kernel(char *const src[], int len)
+{
+  char **ret;
+  int i;
+
+  if (len <= 0)
+    return NULL;
+
+  ret = (char **) kmalloc(sizeof(char *) * len);
+  if (!ret)
+    return NULL;
+
+  for (i = 0; i < len; i++) {
+    if (!src[i]) {
+      ret[i] = NULL;
+      continue;
+    }
+
+    ret[i] = strdup(src[i]);
+    if (!ret[i])
+      return NULL;
+  }
+
+  return ret;
+}
+
+/*
+ * Copy an array from kernel memory to user memory.
+ */
+static char **copy_array_from_kernel_to_user(char **src, int len)
+{
+  int i, slen;
+  char **ret;
+
+  if (!src || len <= 0)
+    return NULL;
+
+  ret = (char **) sys_sbrk(sizeof(char *) * len);
+  memset(ret, 0, sizeof(char *) * len);
+
+  for (i = 0; i < len; i++) {
+    if (!src[i]) {
+      ret[i] = NULL;
+      continue;
+    }
+
+    slen = strlen(src[i]);
+    ret[i] = (char *) sys_sbrk(slen + 1);
+    memset(ret[i], 0, slen + 1);
+    memcpy(ret[i], src[i], slen);
+  }
+
+  return ret;
+}
+
+/*
+ * Free an array of pointers.
+ */
+static void free_array(char **array, int len)
+{
+  int i;
+
+  if (!array)
+    return;
+
+  for (i = 0; i < len; i++)
+    if (array[i])
+      kfree(array[i]);
+
+  kfree(array);
+}
+
+/*
  * Execve system call.
  */
 int sys_execve(const char *path, char *const argv[], char *const envp[])
 {
-  char **kernel_argv = NULL, **kernel_envp = NULL;
-  char **user_argv = NULL, **user_envp = NULL;
-  int ret, i, argv_len, envp_len, len;
+  char **kernel_argv, **kernel_envp;
+  char **user_argv, **user_envp;
+  int ret, argv_len, envp_len;
   uint32_t stack;
 
-  /* get argv and envp size */
+  /* copy argv from user memory to kernel memory */
   argv_len = array_nb_pointers((void *) argv);
+  kernel_argv = copy_array_from_user_to_kernel(argv, argv_len);
+
+  /* copy argv from user memory to kernel memory */
   envp_len = array_nb_pointers((void *) envp);
-
-  /* copy argv in kernel memory */
-  if (argv_len == 0) {
-    kernel_argv = NULL;
-  } else {
-    kernel_argv = (char **) kmalloc(sizeof(char *) * argv_len);
-    if (!kernel_argv) {
-      ret = -ENOMEM;
-      goto err;
-    }
-
-    for (i = 0; i < argv_len; i++)
-      kernel_argv[i] = NULL;
-
-    for (i = 0; i < argv_len; i++) {
-      if (argv[i]) {
-        kernel_argv[i] = strdup(argv[i]);
-        if (!kernel_argv[i]) {
-          ret = -ENOMEM;
-          goto err;
-        }
-      }
-    }
-  }
-
-  /* copy envp in kernel memory */
-  if (envp_len == 0) {
-    kernel_envp = NULL;
-  } else {
-    kernel_envp = (char **) kmalloc(sizeof(char *) * envp_len);
-    if (!kernel_envp) {
-      ret = -ENOMEM;
-      goto err;
-    }
-
-    for (i = 0; i < envp_len; i++)
-      kernel_envp[i] = NULL;
-
-    for (i = 0; i < envp_len; i++) {
-      if (envp[i]) {
-        kernel_envp[i] = strdup(envp[i]);
-        if (!kernel_envp[i]) {
-          ret = -ENOMEM;
-          goto err;
-        }
-      }
-    }
-  }
+  kernel_envp = copy_array_from_user_to_kernel(envp, envp_len);
 
   /* load elf binary */
   ret = elf_load(path);
   if (ret != 0)
-    goto err;
+    goto out;
 
-  /* copy back argv to user address space */
-  user_argv = (char **) sys_sbrk(sizeof(char *) * argv_len);
-  memset(user_argv, 0, sizeof(char *) * argv_len);
-  if (kernel_argv) {
-    for (i = 0; i < argv_len; i++) {
-      if (kernel_argv[i]) {
-        len = strlen(kernel_argv[i]);
-        user_argv[i] = (char *) sys_sbrk(len + 1);
-        memset(user_argv[i], 0, len + 1);
-        memcpy(user_argv[i], kernel_argv[i], len);
-        kfree(kernel_argv[i]);
-      }
-    }
-    kfree(kernel_argv);
-  }
+  /* copy back argv and envp to user address space */
+  user_argv = copy_array_from_kernel_to_user(kernel_argv, argv_len);
+  user_envp = copy_array_from_kernel_to_user(kernel_envp, argv_len);
 
-  /* copy back envp to user address space */
-  user_envp = (char **) sys_sbrk(sizeof(char *) * envp_len);
-  memset(user_envp, 0, sizeof(char *) * envp_len);
-  if (kernel_envp) {
-    for (i = 0; i < envp_len; i++) {
-      if (kernel_envp[i]) {
-        len = strlen(kernel_envp[i]);
-        user_envp[i] = (char *) sys_sbrk(len + 1);
-        memset(user_envp[i], 0, len + 1);
-        memcpy(user_envp[i], kernel_envp[i], len);
-        kfree(kernel_envp[i]);
-      }
-    }
-    kfree(kernel_envp);
-  }
+  /* free kernel memory */
+  free_array(kernel_argv, argv_len);
+  free_array(kernel_envp, envp_len);
 
   /* put argc, argv and envp in kernel stack */
   stack = current_task->user_stack;
@@ -121,22 +128,8 @@ int sys_execve(const char *path, char *const argv[], char *const envp[])
   tss_set_stack(0x10, current_task->kernel_stack);
   enter_user_mode(stack, current_task->user_entry, TASK_RETURN_ADDRESS);
 
-  return 0;
-err:
-  if (kernel_argv) {
-    for (i = 0; i < argv_len; i++)
-      if (kernel_argv[i])
-        kfree(kernel_argv[i]);
-
-    kfree(kernel_argv);
-  }
-
-  if (kernel_envp) {
-    for (i = 0; i < envp_len; i++)
-      if (kernel_envp[i])
-        kfree(kernel_envp[i]);
-
-    kfree(kernel_envp);
-  }
+out:
+  free_array(kernel_argv, argv_len);
+  free_array(kernel_envp, envp_len);
   return ret;
 }
