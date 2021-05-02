b9934f65a0f1010aadedc2954eca2267e1ab589e
eric29200 <eric_begot@yahoo.fr>
2021-05-02 12:13:38 +0200

    ls : use arguments

--- usr/ls.c
+++ usr/ls.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main()
+int main(int argc, char *argv[])
 {
   struct dirent *entry;
+  char *dir_name;
   DIR *dirp;
 
+  /* use arguments or root dir */
+  if (argc > 1)
+    dir_name = argv[1];
+  else
+    dir_name = "/";
+
   /* open directory */
-  dirp = opendir("/");
+  dirp = opendir(dir_name);
   if (dirp == NULL)
     return -1;
 
