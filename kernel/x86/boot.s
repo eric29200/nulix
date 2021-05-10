5bf2cea2e0c392512803ec78becaeae8ce8313cf
eric29200 <eric_begot@yahoo.fr>
2021-05-10 19:08:57 +0200

    boot.s : simplify code

--- kernel/x86/boot.s
+++ kernel/x86/boot.s
@@ -1,42 +1,38 @@
-global loader                               ; the entry symbol for ELF
-global kernel_stack			    ; export kernel stack
+global loader                               	; the entry symbol for ELF
+global kernel_stack			    	; export kernel stack
+extern kmain                                	; the entry symbol for C
 
-extern kmain                                ; the entry symbol for C
-extern __mbHeader
-
-MAGIC_NUMBER  equ 0x1BADB002                ; define the magic number constant
-PAGE_ALIGN    equ 1<<0                      ; Load kernel and modules on a page boundary
-MEM_INFO      equ 1<<1                      ; Provide your kernel with memory info
-FLAGS         equ PAGE_ALIGN | MEM_INFO     	; multiboot flags i.e. ELF
-CHECKSUM      equ -(MAGIC_NUMBER + FLAGS)   	; calculate the checksum
-                                            	; (magic number + checksum + flags should equal 0)
-
-KSTACK_SIZE   equ 0x100000                    	; size of stack = 1 MB
+MAGIC_NUMBER  equ 0x1BADB002                	; define multi boot constants
+PAGE_ALIGN    equ 1<<0
+MEM_INFO      equ 1<<1
+FLAGS         equ PAGE_ALIGN | MEM_INFO
+CHECKSUM      equ -(MAGIC_NUMBER + FLAGS)
+KSTACK_SIZE   equ 0x100000                    	; size of kernel stack = 1 MB
 
 section .__mbHeader                         	; multiboot header
 align 4
-	dd MAGIC_NUMBER                     	; write the magic number to the machine code,
-	dd FLAGS                            	; the flags,
-	dd CHECKSUM                         	; and the checksum
+	dd MAGIC_NUMBER
+	dd FLAGS
+	dd CHECKSUM
 
+section .bss					; kernel stack
+align 4
+kernel_stack:
+	resb KSTACK_SIZE
+
+section .text
+align 4
 loader:                                     	; the loader label (defined as entry point in linker script)
-
 setup_kstack:
 	mov esp, kernel_stack + KSTACK_SIZE     ; point esp to the start of the stack (end of memory area)
 
 call_kmain:
-	push esp				; loads kernel stack address
-	push ebx                                ; loads multiboot header location
-	push eax                                ; loads multiboot magic 0x1BADB002
-	call kmain                              ; this pushes ebp (base pointer) and sets esp
-                                            	; then calls the kmain, which sets 0xBADA55 on eax
-                                            	; it pops ebp and returns
+	push esp				; push kernel stack address
+	push ebx                                ; push multiboot header pointer
+	push eax                                ; push multiboot magic
+	call kmain                              ; call main C function
 
 .loop:
 	hlt					; loop forever
 	jmp .loop
 
-section .bss
-align 4                                     	; align at 4 bytes
-kernel_stack:                               	; label points to beginning of memory
-	resb KSTACK_SIZE                       	; reserve stack for the kernel
