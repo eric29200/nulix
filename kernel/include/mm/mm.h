08ea6abea5c1368bb5e6b91fd385164b4b935458
eric29200 <eric_begot@yahoo.fr>
2021-05-18 19:20:17 +0200

    update user address space limits (in mm.h)

--- kernel/include/mm/mm.h
+++ kernel/include/mm/mm.h
@@ -6,11 +6,17 @@
 
 #define PAGE_SIZE               0x1000
 
-#define KMEM_SIZE               0x600000
-#define KHEAP_START             0x400000
+#define KHEAP_START             0x400000                      /* kernel memory : from 0 to 6 MB */
 #define KHEAP_SIZE              0x200000
+#define KMEM_SIZE               (KHEAP_START + KHEAP_SIZE)
 
-#define USTACK_START            0xF0000000
+#define UHEAP_START             0                             /* user heap : just after text section */
+#define UHEAP_END               0x40000000                    /* can grow until UMAP_START */
+
+#define UMAP_START              0x40000000                    /* user memmory map : from 1 GB to 4 GB */
+#define UMAP_END                0xF0000000
+
+#define USTACK_START            0xF8000000                    /* user stack : 3 Kb at 4 GB*/
 #define USTACK_SIZE             0x8000
 
 void init_mem(uint32_t start, uint32_t end);
