b9523d945f15e197c0fadc1e13f1305862776157
eric29200 <eric_begot@yahoo.fr>
2021-05-24 13:52:23 +0200

    grub : set text mode

--- kernel/x86/boot.s
+++ kernel/x86/boot.s
@@ -5,24 +5,37 @@ extern kmain
 KSTACK_SIZE   equ 0x100000                    	; size of kernel stack = 1 MB
 
 section .multiboot				; multiboot2 section
-align 4
-header_start:
+align 0x1000
+mb_header_start:
 	dd 0xe85250d6
 	dd 0
-	dd header_end - header_start
-	dd -(0xe85250d6 + 0 + (header_end - header_start))
+	dd mb_header_end - mb_header_start
+	dd -(0xe85250d6 + 0 + (mb_header_end - mb_header_start))
+
+align 8
+mb_fb_start:
+	dw 5
+	dw 1
+	dd mb_fb_end - mb_fb_start
+	dd 0
+	dd 0
+	dd 0
+mb_fb_end:
+
+align 8
+mb_tag_end:
 	dw 0
 	dw 0
 	dd 8
-header_end:
+mb_header_end:
 
 section .bss					; kernel stack
-align 4
+align 0x1000
 kernel_stack:
 	resb KSTACK_SIZE
 
 section .text
-align 4
+align 0x1000
 loader:
 	mov esp, kernel_stack + KSTACK_SIZE     ; point esp to the start of the stack (end of memory area)
 
