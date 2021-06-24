74cc6b22925e483e199267c6486747f20ffebb2c
eric29200 <eric_begot@yahoo.fr>
2021-06-24 19:00:54 +0200

    getcwd system call : fix a bug

--- kernel/sys/sys_getcwd.c
+++ kernel/sys/sys_getcwd.c
@@ -8,11 +8,13 @@ int sys_getcwd(char *buf, size_t size)
   size_t n;
 
   /* check size limit */
-  n = strlen(current_task->cwd_path);
+  n = strlen(current_task->cwd_path) + 1;
   if (size < n)
     n = size;
 
   /* get current working dir path */
-  strncpy(buf, current_task->cwd_path, n);
+  strncpy(buf, current_task->cwd_path, n - 1);
+  buf[n] = 0;
+
   return n;
 }
