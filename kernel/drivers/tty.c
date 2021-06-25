b78737f2889093278bb142f17f6db9432cef150f
eric29200 <eric_begot@yahoo.fr>
2021-06-25 20:33:39 +0200

    tty : do not handle canon mode in tty_update()

--- kernel/drivers/tty.c
+++ kernel/drivers/tty.c
@@ -117,45 +117,15 @@ void tty_update(unsigned char c)
     tty->w_pos = 0;
   }
 
-  /* handle carriage return */
-  if (c == 13)
-    c = '\n';
-  else if (c == 127)
-    c = '\b';
+  /* store character */
+  tty->buf[tty->w_pos++] = c;
 
-  /*
-   * Canon mode   = handle special characters and return to read in end of line
-   * Else         = do not handle special characters and return to read at each character
-   */
-  echo = 0;
-  if (L_CANON(tty)) {
-    /* handle new character */
-    if (c == '\b' && tty->w_pos > 0) {
-      tty->buf[tty->w_pos--] = 0;
-      echo = 1;
-    } else if (c != '\b') {
-      tty->buf[tty->w_pos++] = c;
-      echo = 1;
-    }
+  /* echo character on device */
+  if (L_ECHO(tty))
+    tty_write(tty->dev, &c, 1);
 
-    /* echo character on device */
-    if (echo && L_ECHO(tty))
-      tty_write(tty->dev, &c, 1);
-
-    /* wake up eventual process */
-    if (c == '\n')
-      task_wakeup(tty);
-  } else {
-    /* store character */
-    tty->buf[tty->w_pos++] = c;
-
-    /* echo character on device */
-    if (L_ECHO(tty))
-      tty_write(tty->dev, &c, 1);
-
-    /* wake up eventual process */
-    task_wakeup(tty);
-  }
+  /* wake up eventual process */
+  task_wakeup(tty);
 }
 
 /*
