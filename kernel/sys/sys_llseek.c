853dee1cfb5eef1fd86046251541ebe17d6cec89
eric29200 <eric_begot@yahoo.fr>
2021-06-29 16:24:25 +0200

    llseek : remove warnings

--- kernel/sys/sys_llseek.c
+++ kernel/sys/sys_llseek.c
@@ -6,10 +6,9 @@
 int sys_llseek(int fd, uint32_t offset_high, uint32_t offset_low, off_t *result, int whence)
 {
   off_t offset;
-  int ret;
 
   /* compute offset */
-  offset = (offset_high << 32) | offset_low;
+  offset = ((unsigned long long) offset_high << 32) | offset_low;
 
   /* seek */
   *result = do_lseek(fd, offset, whence);
