6c963ea4eb66f93b5b1bec3b5dfb7c49be1a74eb
eric29200 <eric_begot@yahoo.fr>
2021-07-11 20:49:03 +0200

    start pty driver

--- kernel/drivers/pty.c
+++ kernel/drivers/pty.c
@@ -2,12 +2,25 @@
 #include <drivers/tty.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stderr.h>
 
 #define NB_PTYS     4
 
 /* global ptys */
 static struct tty_t pty_table[NB_PTYS];
 static int current_pty = 1;
+
+/*
+ * Lookup for a pty.
+ */
+static struct tty_t *pty_lookup(dev_t dev)
+{
+  /* asked tty */
+  if (minor(dev) > 0 && minor(dev) <= NB_PTYS)
+    return &pty_table[minor(dev) - 1];
+
+  return NULL;
+}
 
 /*
  * Open ptys multiplexer = create a new pty.
@@ -26,7 +39,7 @@ static int ptmx_open(struct file_t *filp)
 }
 
 /*
- * PTY ioctl.
+ * PTY multiplexer ioctl.
  */
 int ptmx_ioctl(struct file_t *filp, int request, unsigned long arg)
 {
@@ -38,6 +51,52 @@ int ptmx_ioctl(struct file_t *filp, int request, unsigned long arg)
       break;
     default:
       printf("Unknown ioctl request (%x) on device %x\n", request, DEV_PTMX);
+      break;
+  }
+
+  return 0;
+}
+
+/*
+ * PTY ioctl.
+ */
+int pty_ioctl(struct file_t *filp, int request, unsigned long arg)
+{
+  struct tty_t *tty;
+  dev_t dev;
+
+  /* get tty */
+  dev = filp->f_inode->i_zone[0];
+  tty = pty_lookup(dev);
+  if (!tty)
+    return -EINVAL;
+
+  printf("ok\n");
+
+  switch (request) {
+    case TCGETS:
+      memcpy((struct termios_t *) arg, &tty->termios, sizeof(struct termios_t));
+      break;
+    case TCSETS:
+      memcpy(&tty->termios, (struct termios_t *) arg, sizeof(struct termios_t));
+      break;
+    case TCSETSW:
+      memcpy(&tty->termios, (struct termios_t *) arg, sizeof(struct termios_t));
+      break;
+    case TCSETSF:
+      memcpy(&tty->termios, (struct termios_t *) arg, sizeof(struct termios_t));
+      break;
+    case TIOCGWINSZ:
+      memcpy((struct winsize_t *) arg, &tty->winsize, sizeof(struct winsize_t));
+      break;
+    case TIOCGPGRP:
+      *((pid_t *) arg) = tty->pgrp;
+      break;
+    case TIOCSPGRP:
+      tty->pgrp = *((pid_t *) arg);
+      break;
+    default:
+      printf("Unknown ioctl request (%x) on device %x\n", request, dev);
       break;
   }
 
@@ -90,3 +149,16 @@ struct inode_operations_t ptmx_iops = {
   .fops           = &ptmx_fops,
 };
 
+/*
+ * Pty file operations.
+ */
+static struct file_operations_t pty_fops = {
+  .ioctl        = pty_ioctl,
+};
+
+/*
+ * Pty inode operations.
+ */
+struct inode_operations_t pty_iops = {
+  .fops         = &pty_fops,
+};
--- kernel/include/drivers/pty.h
+++ kernel/include/drivers/pty.h
@@ -4,5 +4,6 @@
 #include <fs/fs.h>
 
 extern struct inode_operations_t ptmx_iops;
+extern struct inode_operations_t pty_iops;
 
 #endif
