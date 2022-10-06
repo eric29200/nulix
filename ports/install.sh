aead265015ee9d073fa3ec1926a58d8eedbb5c30
eric29200 <eric_begot@yahoo.fr>
2022-10-06 08:53:34 +0200

    remove openssl port

--- ports/install.sh
+++ ports/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # base ports, needed to build other ports
-BASE_PORTS=("pkgconf" "bash" "libncurses" "zlib" "openssl")
+BASE_PORTS=("pkgconf" "bash" "libncurses" "zlib")
 
 ################################
 ######### install musl #########
