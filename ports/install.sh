fc8715b068805e1c13e15d69ea7ef96fb51a27d5
eric29200 <eric_begot@yahoo.fr>
2022-10-19 17:22:08 +0200

    port expat, fontconfig and freetype

--- ports/install.sh
+++ ports/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # base ports, needed to build other ports
-BASE_PORTS=("pkgconf" "bash" "libncurses" "zlib" "libpng")
+BASE_PORTS=("pkgconf" "bash" "libncurses" "zlib" "libpng" "freetype" "expat" "fontconfig")
 
 if [[ `basename $PWD` != "nulix" ]]; then
 	echo "This script must be run from main/root directory"
