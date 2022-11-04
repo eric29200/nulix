a718f1d83050281f687c47d7026770903d67e852
eric29200 <eric_begot@yahoo.fr>
2022-11-04 17:44:54 +0100

    sys/sys_clock_gettime64.c : fix CLOCK_REALTIME and CLOCK_MONOTONIC

--- kernel/sys/sys_clock_gettime64.c
+++ kernel/sys/sys_clock_gettime64.c
@@ -9,12 +9,11 @@ int sys_clock_gettime64(clockid_t clockid, struct timespec_t *tp)
 {
 	switch (clockid) {
 		case CLOCK_REALTIME:
-			tp->tv_sec = CURRENT_TIME;
-			tp->tv_nsec = 0;
+			jiffies_to_timespec(jiffies, tp);
+			tp->tv_sec += startup_time;
 			break;
 		case CLOCK_MONOTONIC:
-			tp->tv_sec = jiffies / HZ;
-			tp->tv_nsec = 0;
+			jiffies_to_timespec(jiffies, tp);
 			break;
 		default:
 			printf("clock_gettime64 not implement on clockid=%d\n", clockid);
