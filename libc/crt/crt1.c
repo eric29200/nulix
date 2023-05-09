e201a11486fd2659bf1291457fd05b6cd6480c51
eric29200 <eric_begot@yahoo.fr>
2023-05-09 20:53:23 +0200

    libc : fix crt1.c

---
+++ libc/crt/crt1.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "../src/stdio/__stdio_impl.h"
+
+#define START "_start"
+
+#include "crt0.h"
+
+extern int main(int, char **, char **);
+
+void _start_c(long *p)
+{
+	char **argv, **envp;
+	int argc, code;
+
+	/* get argc, argv and envp */	
+	argc = p[0];
+	argv = (void *) (p + 1);
+	envp = argv + argc + 1;
+
+	/* init stdio */
+	environ = envp;
+	__stdio_init();
+
+	/* execute main */
+	code = main(argc, argv, envp);
+
+	/* exit */
+	exit(code);
+}
