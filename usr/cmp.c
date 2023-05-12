80bb3894e612b438654ba2b38a9db0bf10b78f29
eric29200 <eric_begot@yahoo.fr>
2023-05-13 00:29:00 +0200

    usr : implement cmp.c

---
+++ usr/cmp.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+	char buf1[BUFSIZ], buf2[BUFSIZ];
+	int fd1 = -1, fd2 = -1;
+	ssize_t n, n1, n2, pos;
+	int ret, i;
+
+	/* check arguments */
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s file1 file2\n", argv[0]);
+		return EINVAL;
+	}
+
+	/* open file1 */
+	fd1 = open(argv[1], 0);
+	if (fd1 < 0) {
+		ret = errno;
+		perror(argv[1]);
+		goto err;
+	}
+
+	/* open file2 */
+	fd2 = open(argv[2], 0);
+	if (fd2 < 0) {
+		ret = errno;
+		perror(argv[2]);
+		goto err;
+	}
+
+	for (pos = 0;;) {
+		/* read file1 */
+		n1 = read(fd1, buf1, BUFSIZ);
+		if (n1 < 0) {
+			perror(argv[1]);
+			goto err;
+		}
+
+		/* read file2 */
+		n2 = read(fd2, buf2, BUFSIZ);
+		if (n2 < 0) {
+			perror(argv[2]);
+			goto err;
+		}
+
+		/* end of files */
+		if (!n1 && !n2)
+			break;
+
+		/* compare buffers */
+		if (n1 == n2 && memcmp(buf1, buf2, n1) == 0) {
+			pos += n1;
+			continue;
+		}
+
+		/* find difference */
+		n = n1 < n2 ? n1 : n2;
+		for (i = 0; i < n; i++, pos++)
+			if (buf1[i] != buf2[i])
+				break;
+
+		fprintf(stderr, "%s %s are different: byte %d\n", argv[0], argv[1], pos);
+		ret = 1;
+		goto err;
+	}
+
+	ret = 0;
+err:
+	if (fd1 > 0)
+		close(fd1);
+	if (fd2 > 0)
+		close(fd2);
+	return ret;
+}
