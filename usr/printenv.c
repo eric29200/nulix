e7ca08af5264b9f1ec059f0842133b596ac86a7c
eric29200 <eric_begot@yahoo.fr>
2023-05-13 23:55:31 +0200

    usr : implement printenv.c

---
+++ usr/printenv.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	char **env = environ;
+	char *eptr;
+	size_t len;
+
+	/* print all */
+	if (argc == 1) {
+		while (*env) {
+			eptr = *env++;
+			printf("%s\n", eptr);
+		}
+
+		return 0;
+	}
+
+	/* find variable */
+	len = strlen(argv[1]);
+	while (*env) {
+		if (strlen(*env) > len && env[0][len] == '=' && memcmp(argv[1], *env, len) == 0) {
+			eptr = &env[0][len + 1];
+			printf("%s\n", eptr);
+			return 0;
+		}
+
+		env++;
+	}
+
+	return EXIT_FAILURE;
+}
