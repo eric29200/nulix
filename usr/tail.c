6265164c75e8bb951156840fb54479b8ab177aa3
eric29200 <eric_begot@yahoo.fr>
2023-05-19 11:27:06 +0200

    usr/head.c and usr/tail.c : use getopt to parse options

--- usr/tail.c
+++ usr/tail.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "libutils/utils.h"
+
+#define NR_LINES_DEFAULT	10
 
 /*
  * Tail a file.
@@ -54,31 +60,78 @@ static int tail(FILE *fp, const char *filename, size_t nr_lines)
 static void usage(const char *name)
 {
 	fprintf(stderr, "Usage: [-n] [file ...]\n", name);
+	fprintf(stderr, "\t  , --help\t\t\tprint help and exit\n");
+	fprintf(stderr, "\t-n, --lines=N\t\t\tprint N last lines\n");
 }
+
+/* options */
+struct option long_opts[] = {
+	{ "help",	no_argument,		0,	OPT_HELP	},
+	{ "lines",	required_argument,	0,	'n'		},
+	{ 0,		no_argument,		0,	'0'		},
+	{ 0,		no_argument,		0,	'1'		},
+	{ 0,		no_argument,		0,	'2'		},
+	{ 0,		no_argument,		0,	'3'		},
+	{ 0,		no_argument,		0,	'4'		},
+	{ 0,		no_argument,		0,	'5'		},
+	{ 0,		no_argument,		0,	'6'		},
+	{ 0,		no_argument,		0,	'7'		},
+	{ 0,		no_argument,		0,	'8'		},
+	{ 0,		no_argument,		0,	'9'		},
+	{ 0,		0,			0,	0		},
+};
 
 int main(int argc, char **argv)
 {
 	bool several = false, nl = false;
 	const char *name = argv[0];
-	size_t nr_lines = 10;
-	int ret = 0;
+	bool nr_lines_set = false;
+	size_t nr_lines = 0;
+	int ret = 0, c;
 	FILE *fp;
 
-	/* skip arg[0] */
-	argc--;
-	argv++;
+	/* get options */
+	while ((c = getopt_long(argc, argv, "0123456789n:", long_opts, NULL)) != -1) {
+		switch (c) {
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				nr_lines = nr_lines * 10 + c - '0';
+				nr_lines_set = true;
+				break;
+			case 'n':
+			 	nr_lines = atoi(optarg);
+				if (nr_lines == 0 && strcmp(optarg, "0") != 0) {
+					fprintf(stderr, "%s: incorrect number of lines '%s'\n", name, optarg);
+					exit(1);
+				}
 
-	/* parse options */
-	if (argc > 0 && argv[0][0] == '-') {
-		nr_lines = atoi(&argv[0][1]);
-		if (nr_lines <= 0) {
-			usage(name);
-			exit(1);
+				nr_lines_set = true;
+				break;
+			case OPT_HELP:
+				usage(name);
+				exit(0);
+				break;
+			default:
+				exit(1);
+				break;
 		}
-
-		argc--;
-		argv++;
 	}
+
+	/* skip options */
+	argc -= optind;
+	argv += optind;
+
+	/* default number or lines */
+	if (!nr_lines_set)
+		nr_lines = NR_LINES_DEFAULT;
 
 	/* no file : stdin */
 	if (!argc)
