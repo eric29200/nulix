2cf2229829d5e90b20608b7ef1c080fd7dfb1075
eric29200 <eric_begot@yahoo.fr>
2025-04-06 23:08:22 +0200

    kernel/proc/sched.c: scheduler fix

--- kernel/proc/sched.c
+++ kernel/proc/sched.c
@@ -105,25 +105,6 @@ static void update_process_times()
 }
 
 /*
- * Update old timers.
- */
-static void update_old_timers()
-{
-	struct list_head *pos;
-	struct task *task;
-
-	list_for_each(pos, &tasks_list) {
-		task = list_entry(pos, struct task, list);
-		if (task && task->timeout && task->timeout < jiffies) {
-			task->timeout = 0;
-
-			if (task->state == TASK_SLEEPING)
-				wake_up_process(task);
-		}
-	}
-}
-
-/*
  * Handle timer (PIT) interrupt.
  */
 void do_timer_interrupt()
@@ -132,7 +113,10 @@ void do_timer_interrupt()
 	update_times();
 	update_process_times();
 	update_timers();
-	update_old_timers();
+
+	/* reschedule if needed */
+	if (need_resched)
+		schedule();
 }
 
 /*
@@ -175,16 +159,23 @@ void schedule()
 	need_resched = 0;
 
 	/* check previous task */
-	if (prev && prev->state == TASK_SLEEPING) {
-		if (signal_pending(prev)) {
-			prev->state = TASK_RUNNING;
-		} else {
+	switch (prev->state) {
+		case TASK_SLEEPING:
+			/* signal pending : make it runnable */
+			if (signal_pending(prev)) {
+				prev->state = TASK_RUNNING;
+				break;
+			}
+
+			/* timeout expired : make it runnable */
 			timeout = prev->timeout;
 			if (timeout && timeout <= jiffies) {
 				prev->timeout = 0;
 				timeout = 0;
+				prev->state = TASK_RUNNING;
 			}
-		}
+
+			break;
 	}
 
 	/* choose next task to run */
