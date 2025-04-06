791df127f0fae9545b4ddedb087adda909ebb487
eric29200 <eric_begot@yahoo.fr>
2025-04-06 10:58:46 +0200

    typo fix

--- kernel/proc/sched.c
+++ kernel/proc/sched.c
@@ -128,7 +128,6 @@ static void update_old_timers()
  */
 void do_timer_interrupt()
 {
-
 	/* update times and timers */
 	update_times();
 	update_process_times();
@@ -168,7 +167,7 @@ void schedule()
 	/* save current task */
 	prev = current_task;
 	need_resched = 0;
-	
+
 	/* choose next task to run */
 	next = kinit_task;
 	next_counter = -1000;
