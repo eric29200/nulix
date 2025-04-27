5e85a1d5636499a5c3accfcf174a39db4f110b40
eric29200 <eric_begot@yahoo.fr>
2025-04-27 10:21:50 +0200

    kernel/x86: code cleanup

--- kernel/include/x86/system.h
+++ kernel/include/x86/system.h
@@ -3,12 +3,6 @@
 
 #include <stddef.h>
 
-#define KERNEL_CODE_SEGMENT	0x08
-#define KERNEL_DATA_SEGMENT	0x10
-#define USER_CODE_SEGMENT	0x18
-#define USER_DATA_SEGMENT	0x20
-#define TSS_SEGMENT		0x28
-
 struct registers {
 	uint32_t ds;
 	uint32_t edi;
