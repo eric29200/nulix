61a4565fd8019c2b53c255ae99c1f73aabfedc9e
eric29200 <eric_begot@yahoo.fr>
2025-10-05 10:54:50 +0200

    kernel/include/x86/bitops.h: fix bit operations

--- kernel/include/x86/bitops.h
+++ kernel/include/x86/bitops.h
@@ -1,23 +1,58 @@
 #ifndef _BITOPS_H_
 #define _BITOPS_H_
 
-static inline int test_bit(uint32_t *addr, int bit)
+/*
+ * Test a bit.
+ */
+static inline int test_bit(uint32_t *addr, uint32_t bit)
 {
-	return (*addr & (1UL << bit)) != 0;
+	int mask;
+
+	addr += bit >> 5;
+	mask = 1 << (bit & 0x1F);
+	return ((mask & *addr) != 0);
 }
 
-static inline int set_bit(uint32_t *addr, int bit)
+/*
+ * Set a bit.
+ */
+static inline int set_bit(uint32_t *addr, uint32_t bit)
 {
-	int old = test_bit(addr, bit);
-	*addr |= 1UL << bit;
-	return old;
+	int mask, ret;
+
+	addr += bit >> 5;
+	mask = 1 << (bit & 0x1F);
+	ret = (mask & *addr) != 0;
+	*addr |= mask;
+	return ret;
 }
 
-static inline int clear_bit(uint32_t *addr, int bit)
+/*
+ * Clear a bit.
+ */
+static inline int clear_bit(uint32_t *addr, uint32_t bit)
 {
-	int old = test_bit(addr, bit);
-	*addr &= ~(1UL << bit);
-	return old;
+	int mask, ret;
+
+	addr += bit >> 5;
+	mask = 1 << (bit & 0x1F);
+	ret = (mask & *addr) != 0;
+	*addr &= ~mask;
+	return ret;
+}
+
+/*
+ * Find first zero bit.
+ */
+static inline uint32_t find_first_zero_bit(uint32_t *addr, size_t limit)
+{
+	uint32_t bit;
+
+	for (bit = 0; bit < limit; bit++)
+		if (!test_bit(addr, bit))
+			return bit;
+
+	return -1;
 }
 
 #endif
