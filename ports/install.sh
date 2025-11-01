698d0a47e86b8558e6f372ff64c2879bb19ffd8a
eric29200 <eric_begot@yahoo.fr>
2025-11-01 20:16:24 +0100

    port libpipeline

--- ports/install.sh
+++ ports/install.sh
@@ -3,7 +3,7 @@
 # base ports, needed to build other ports
 BASE_PORTS=("pkgconf" "libncurses" "termcap" "zlib" "bzip2" "openssl" "util-linux"	\
 	"libpng" "libjpeg" "libgif" "expat" "freetype2" "fontconfig" "liberation"	\
-	"tinyx" "imlib2" "argp-standalone" "musl-fts" "musl-obstack")
+	"tinyx" "imlib2" "argp-standalone" "musl-fts" "musl-obstack" "libpipeline")
 
 if [[ `basename $PWD` != "nulix" ]]; then
 	echo "This script must be run from main/root directory"
