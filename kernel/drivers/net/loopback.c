e3f2f28b8f06dbff4b11d4cbbf6b2e83e86dfa29
eric29200 <eric_begot@yahoo.fr>
2026-08-25 22:10:54 +0200

    kernel/drivers/net/loopback.c: fix loopback driver

--- kernel/drivers/net/loopback.c
+++ kernel/drivers/net/loopback.c
@@ -6,25 +6,22 @@
 #include <stderr.h>
 #include <stdio.h>
 
-/* Loopback device */
-static struct net_device *loopback_dev = NULL;
-
 /*
  * Send a socket buffer.
  */
-static int loopback_start_xmit(struct sk_buff *skb, struct net_device *dev)
+static int loopback_start_xmit(struct sk_buff *skb, struct net_device *net_dev)
 {
 	/* set network device */
-	skb->dev = loopback_dev;
+	skb->dev = net_dev;
 
 	/* decode ethernet header */
 	ethernet_receive(skb);
 
 	/* update transmit and receive stats */
-	dev->stats.tx_packets++;
-	dev->stats.tx_bytes += skb->size;
-	dev->stats.rx_packets++;
-	dev->stats.rx_bytes += skb->size;
+	net_dev->stats.tx_packets++;
+	net_dev->stats.tx_bytes += skb->size;
+	net_dev->stats.rx_packets++;
+	net_dev->stats.rx_bytes += skb->size;
 
 	/* receive packet */
 	netif_rx(skb);
@@ -37,20 +34,22 @@ static int loopback_start_xmit(struct sk_buff *skb, struct net_device *dev)
  */
 int init_loopback()
 {
+	struct net_device *net_dev;
+
 	/* register net device */
-	loopback_dev = register_net_device(0, ARPHRD_ETHER, AF_INET, "lo");
-	if (!loopback_dev)
+	net_dev = register_net_device(0, ARPHRD_ETHER, AF_INET, "lo");
+	if (!net_dev)
 		return -ENOSPC;
 
 	/* set device */
-	loopback_dev->addr_len = ETHERNET_ALEN;
-	loopback_dev->hard_header_len = ETHERNET_HLEN;
-	loopback_dev->hard_header = ethernet_header;
-	loopback_dev->rebuild_header = ethernet_rebuild_header;
-	loopback_dev->start_xmit = loopback_start_xmit;
-	loopback_dev->ip_addr = in_aton("127.0.0.1");
-	loopback_dev->ip_netmask = in_aton("255.0.0.0");
-	loopback_dev->flags = IFF_LOOPBACK;
+	net_dev->addr_len = ETHERNET_ALEN;
+	net_dev->hard_header_len = ETHERNET_HLEN;
+	net_dev->hard_header = ethernet_header;
+	net_dev->rebuild_header = ethernet_rebuild_header;
+	net_dev->start_xmit = loopback_start_xmit;
+	net_dev->ip_addr = in_aton("127.0.0.1");
+	net_dev->ip_netmask = in_aton("255.0.0.0");
+	net_dev->flags = IFF_LOOPBACK;
 
 	return 0;
 }
