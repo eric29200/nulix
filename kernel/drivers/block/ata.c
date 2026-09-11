744628fd1e6b7b35fb117b23a9d25cbdd22203da
eric29200 <eric_begot@yahoo.fr>
2026-09-11 22:50:10 +0200

    kernel/drivers/block/ata.c: remove irq handler

--- kernel/drivers/block/ata.c
+++ kernel/drivers/block/ata.c
@@ -272,14 +272,6 @@ static int ata_ioctl(struct inode *inode, struct file *filp, int request, unsign
 }
 
 /*
- * Irq handler.
- */
-static void ata_irq_handler(struct registers *regs)
-{
-	UNUSED(regs);
-}
-
-/*
  * ATA file operations.
  */
 static struct file_operations ata_fops = {
@@ -307,14 +299,6 @@ static int ata_probe(struct pci_device *pci_dev, struct pci_device_id *id)
 	bar4 = pci_read_field(pci_dev->address, PCI_BAR4);
 	if (bar4 & 0x00000001)
 		bar4 &= 0xFFFFFFFC;
-
-	/* register interrupt handlers */
-	ret = request_irq(ATA_PRIMARY_IRQ, ata_irq_handler, 0, "primary hd", NULL);
-	if (ret)
-		return ret;
-	ret = request_irq(ATA_SECONDARY_IRQ, ata_irq_handler, 0, "secondary hd", NULL);
-	if (ret)
-		return ret;
 
 	/* register ata device */
 	ret = register_blkdev(DEV_ATA_MAJOR, "ata", &ata_fops);
