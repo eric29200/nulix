643e3d37c0abf5686c097b95a08f5f71ead2b449
eric29200 <eric_begot@yahoo.fr>
2026-09-14 20:57:35 +0200

    drivers/block/ide_hd.c: fix IDE disk driver

--- kernel/drivers/block/ide_hd.c
+++ kernel/drivers/block/ide_hd.c
@@ -6,43 +6,12 @@
 #define NR_DMA_SECTORS			((IDE_NR_DMA_PAGES) * PAGE_SIZE / ATA_SECTOR_SIZE)
 
 /*
- * Issue read/write command.
- */
-static void __ide_do_rw_disk(struct ide_drive *drive, int cmd, uint32_t sector, size_t nr_sectors)
-{
-	int status, dstatus;
-
-	/* select sector */
-	outb(drive->io_base + ATA_REG_CONTROL, 0x00);
-	outb(drive->io_base + ATA_REG_HDDEVSEL, (drive->drive == ATA_MASTER ? 0xE0 : 0xF0) | ((sector >> 24) & 0x0F));
-	outb(drive->io_base + ATA_REG_FEATURES, 0x00);
-	outb(drive->io_base + ATA_REG_SECCOUNT0, nr_sectors);
-	outb(drive->io_base + ATA_REG_LBA0, (uint8_t) sector);
-	outb(drive->io_base + ATA_REG_LBA1, (uint8_t) (sector >> 8));
-	outb(drive->io_base + ATA_REG_LBA2, (uint8_t) (sector >> 16));
-
-	/* issue dma command */
-	ide_dmaproc(drive, cmd, nr_sectors * ATA_SECTOR_SIZE);
-
-	/* wait for completion */
-	for (;;) {
-		status = inb(drive->hwif->dma_base + 2);
-		dstatus = inb(drive->io_base + ATA_REG_STATUS);
-
-		if (!(status & 0x04))
-			continue;
-
-		if (!(dstatus & ATA_SR_BSY))
-			break;
-	}
-}
-
-/*
  * Do read/write.
  */
 int ide_do_rw_disk(struct ide_drive *drive, struct request *req)
 {
 	uint32_t start_sector, sector, nr_sectors, nsect;
+	int status, dstatus;
 	char *buf = req->buf;
 
 	/* get partition start sector */
@@ -67,8 +36,29 @@ int ide_do_rw_disk(struct ide_drive *drive, struct request *req)
 		if (req->cmd == WRITE)
 			memcpy(drive->buf, buf, nsect * ATA_SECTOR_SIZE);
 
-		/* issue read/write command */
-		__ide_do_rw_disk(drive, req->cmd == READ ? ATA_CMD_READ_DMA : ATA_CMD_WRITE_DMA, sector, nsect);
+		/* select sector */
+		outb(drive->io_base + ATA_REG_CONTROL, 0x00);
+		outb(drive->io_base + ATA_REG_HDDEVSEL, (drive->drive == ATA_MASTER ? 0xE0 : 0xF0) | ((sector >> 24) & 0x0F));
+		outb(drive->io_base + ATA_REG_FEATURES, 0x00);
+		outb(drive->io_base + ATA_REG_SECCOUNT0, nr_sectors);
+		outb(drive->io_base + ATA_REG_LBA0, (uint8_t) sector);
+		outb(drive->io_base + ATA_REG_LBA1, (uint8_t) (sector >> 8));
+		outb(drive->io_base + ATA_REG_LBA2, (uint8_t) (sector >> 16));
+
+		/* issue dma command */
+		ide_dmaproc(drive, req->cmd == READ ? ATA_CMD_READ_DMA : ATA_CMD_WRITE_DMA, nr_sectors * ATA_SECTOR_SIZE);
+
+		/* wait for completion */
+		for (;;) {
+			status = inb(drive->hwif->dma_base + 2);
+			dstatus = inb(drive->io_base + ATA_REG_STATUS);
+
+			if (!(status & 0x04))
+				continue;
+
+			if (!(dstatus & ATA_SR_BSY))
+				break;
+		}
 
 		/* copy buffer */
 		if (req->cmd == READ)
